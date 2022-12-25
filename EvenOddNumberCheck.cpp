c991c8effe6f0d98694e414503475851591a7758
siddharthagrahari978 <siddharthagrahari978@gmail.com>
2022-12-26 01:23:57 +0530

    First Commit

---
+++ EvenOddNumberCheck.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int num;
+  cin >> num;
+  if (num % 2 == 0)
+  {
+    cout << "Even Number";
+  }
+  else if (num == 69)
+  {
+    cout << "Unknown Practice";
+  }
+  else
+  {
+    cout << "Odd Number";
+  }
+}
